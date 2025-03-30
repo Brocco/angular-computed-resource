54590158602743d7cce82640a2e21e2ea29f68b7
Mike Brocchi <brocco@users.noreply.github.com>
2025-03-30 16:06:37 -0400

    take typings from play function to real implementation

--- libs/computed-resource/src/lib/computed-resource.ts
+++ libs/computed-resource/src/lib/computed-resource.ts
@@ -1,158 +1,126 @@
-import {
-  computed,
-  resource,
-  Resource,
-  ResourceRef,
-  ResourceStatus,
-  Signal,
-} from '@angular/core';
-import { k } from '@angular/core/weak_ref.d-Bp6cSy-X';
+import { computed, Resource, ResourceStatus, Signal } from '@angular/core';
 
-const resA: Resource<string | undefined> = resource({
-  loader: () => Promise.resolve('A'),
-});
-const resB = resource({
-  loader: () => Promise.resolve(2),
-});
-
-const map = {
-  a: resA,
-  b: resB,
-};
-const output: {
-  a: ReturnType<(typeof map)['a']['value']>;
-  b: ReturnType<(typeof map)['b']['value']>;
-} = {
-  a: resA.value(),
-  b: resB.value(),
-};
-
-type ResourceMap = {
-  [key: string]: Resource<unknown>;
-};
-
-export function computedResourceOld(resources: ResourceMap) {
-  type KEY = Exclude<keyof typeof resources, number>;
-  type VALS = (typeof resources)[KEY] extends Resource<infer VALS>
-    ? VALS
-    : never;
-
-  const value = computed(() => {
-    // const result: { [key: KEY]: VALS } = {};
-    const result: { [key: string]: unknown } = {};
-
-    // create a computed value for each of the resources in the map
-    for (const [key, resource] of Object.entries(resources)) {
-      type VAL = typeof resource extends Resource<infer V> ? V : unknown;
-      // type VAL = ReturnType<typeof resource.value>;
-      result[key] = resource.value() as VAL;
-    }
-
-    return result;
-  });
-
-  return {
-    value,
-  };
-}
-
-// check if returned value matches expecting value() to be:
-// { a: string; b: number; } OR
-// { a: string | undefined; b: number | undefined; }
-const check: { a: string | undefined; b: number | undefined } =
-  computedResourceOld(map).value();
-
-console.log(check);
-
-function extractResourceValues<T extends ResourceMap>(
-  resources: T
-): {
-  value: () => { [K in keyof T]: T[K] extends Resource<infer V> ? V : never };
-} {
-  const values: Partial<{
-    [K in keyof T]: T[K] extends Resource<infer V> ? V : never;
-  }> = {};
-
-  // type VAL = T[keyof T] extends Resource<infer V> ? V : never };
-
-  for (const [key, resource] of Object.entries(resources)) {
-    values[key as keyof T] = resource.value() as T[keyof T] extends Resource<
-      infer V
-    >
-      ? V
-      : never;
-  }
-
-  return {
-    value: () =>
-      values as {
-        [K in keyof T]: T[K] extends Resource<infer V> ? V : never;
-      },
-  };
-}
-const check2 = extractResourceValues(map).value();
-
-type VAL<T> = T[keyof T] extends Resource<infer V> ? V : never;
-
-// const x: Resource<string>;
-
-/*
-{
-  value: () => { [K in keyof T]: T[K] extends Resource<infer V> ? V : never };
-}
-*/
-// function computedResource<T>(resources: {
-type MyResource<T> = Pick<Resource<T>, 'value'>;
-// type MyResource<T> = {
-//   value: () => T;
-// };
-
-function computedResourceX<
+export function computedResource<
   T extends { [key: string]: Resource<unknown> },
-  O extends { [K in keyof T]: T[K] extends Resource<infer V> ? V : never }
->(resources: T): MyResource<O> {
-  const value = computed(() => {
-    const result = Object.keys(resources).reduce((acc, key) => {
-      acc[key as keyof O] = resources[key].value() as O[keyof O];
-      return acc;
-    }, {} as Partial<O>);
+  V = { [K in keyof T]: T[K] extends Resource<infer V> ? V : never }
+>(resources: T, options?: ComputedResourceOptions): ComputedResource<T, V> {
+  return new ComputedResourceImpl<T, V>(resources, options);
+}
 
-    return result as O;
+export interface ComputedResourceOptions {
+  isLoadingStrategy: 'some' | 'all';
+  hasValueStrategy: 'some' | 'all';
+}
+
+export interface ComputedResource<
+  T extends { [key: string]: Resource<unknown> },
+  V = { [K in keyof T]: T[K] extends Resource<infer V> ? V : never }
+> extends Resource<V> {
+  partialValue: (key: keyof V) => Signal<V[keyof V]> | undefined;
+  partialReload: (key: keyof V) => boolean | undefined;
+}
+
+class ComputedResourceImpl<
+  T extends { [key: string]: Resource<unknown> },
+  V = { [K in keyof T]: T[K] extends Resource<infer V> ? V : never }
+> implements ComputedResource<T, V>
+{
+  private readonly options?: ComputedResourceOptions;
+  private readonly resources: T;
+  private readonly computedResourceOptionsDefault: ComputedResourceOptions = {
+    isLoadingStrategy: 'all',
+    hasValueStrategy: 'all',
+  };
+
+  constructor(resources: T, options?: ComputedResourceOptions) {
+    this.resources = resources;
+    this.options = {
+      ...this.computedResourceOptionsDefault,
+      ...(options ?? {}), // override default options with user-provided options
+    };
+  }
+
+  // get a single resource's value
+  partialValue(key: keyof V): Signal<V[keyof V]> | undefined {
+    return this.resources[key as keyof T]?.value as Signal<V[keyof V]>;
+  }
+
+  readonly value = computed(() => {
+    const result = Object.keys(this.resources).reduce((acc, key) => {
+      acc[key as keyof V] = this.resources[key].value() as V[keyof V];
+      return acc;
+    }, {} as Partial<V>);
+
+    return result as V;
   });
 
-  return {
-    value: value,
-  };
-}
-const xxx = computedResourceX(map);
-xxx.value().a = 'b';
-xxx.value().b = 3;
+  readonly isLoading =
+    this.options?.isLoadingStrategy === 'some'
+      ? computed(() => !Object.values(this.resources).some((resource) => !resource.isLoading()))
+      : computed(() => !Object.values(this.resources).every((resource) => !resource.isLoading()));
 
-function computedResource<
-  V,
-  T extends { [K in keyof T]: T[K] extends Resource<infer V> ? V : never }
->(resources: { [key: string]: Resource<unknown> }): Resource<V> {
-  // }): Resource<{
-  //   [K in keyof T]: T[K] extends Resource<infer V> ? V : never;
-  // }> {
-  const values: Partial<{
-    [K in keyof T]: V;
-  }> = {};
-  for (const [key, resource] of Object.entries(resources)) {
-    values[key as keyof T] = resource.value() as V;
-    // values[key as keyof T] = resource.value() as T[keyof T] extends Resource<
-    //   infer V
-    // >
-    //   ? V
-    //   : never;
+  hasValue(): this is Resource<Exclude<V, undefined>> {
+    // return this.value() !== undefined;
+    return this.options?.hasValueStrategy === 'some'
+      ? Object.values(this.resources).some((resource) => resource.value() !== undefined)
+      : Object.values(this.resources).every((resource) => resource.hasValue() !== undefined);
   }
-  return {
-    value: () =>
-      values as {
-        [K in keyof T]: T[K] extends Resource<infer V> ? V : never;
-      },
-  };
-}
 
-const shouldBe = computedResource(map);
-console.log(shouldBe.value()); // should be { a: string | undefined; b: number | undefined; }
+  /**
+   * TODO: Review this implementation
+   */
+  readonly status = computed(() => {
+    const statusCounts = Object.values(this.resources).reduce((acc, resource) => {
+      const count = acc.get(resource.status()) || 0;
+      acc.set(resource.status(), count + 1);
+      return acc;
+    }, new Map());
+
+    if (statusCounts.get(ResourceStatus.Error) || 0 > 0) {
+      return ResourceStatus.Error;
+    }
+    if (statusCounts.get(ResourceStatus.Reloading) || 0 > 0) {
+      return ResourceStatus.Reloading;
+    }
+    if (statusCounts.get(ResourceStatus.Loading) || 0 > 0) {
+      return ResourceStatus.Loading;
+    }
+    if (statusCounts.get(ResourceStatus.Idle) || 0 === Object.keys(this.resources).length) {
+      return ResourceStatus.Idle;
+    }
+    if (statusCounts.get(ResourceStatus.Resolved) || 0 === Object.keys(this.resources).length) {
+      return ResourceStatus.Resolved;
+    }
+    return ResourceStatus.Local;
+  });
+
+  readonly error = computed(() => {
+    return Object.entries(this.resources).reduce((acc, [key, resource]) => {
+      if (!resource.error()) {
+        return acc;
+      }
+      if (acc === undefined) {
+        acc = {};
+      }
+      acc[key] = resource.error();
+      return acc;
+    }, undefined as { [key: string]: unknown } | undefined);
+  });
+
+  /**
+   * TODO: Review this implementation
+   *   - can we combine all results of reload calls?
+   *   - what if some resources fail to reload?
+   *   - do we care?
+   */
+  reload() {
+    for (const resource of Object.values(this.resources)) {
+      resource.reload();
+    }
+    return true;
+  }
+
+  partialReload(key: keyof V): boolean | undefined {
+    return this.resources[key as keyof T]?.reload();
+  }
+}
