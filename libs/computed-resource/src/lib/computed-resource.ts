3ec5cfdb93fc593ebad10030408d640193117d7e
Mike Brocchi <brocco@users.noreply.github.com>
2025-03-30 15:26:17 -0400

    working now via computedResourceX - needs cleanup

--- libs/computed-resource/src/lib/computed-resource.ts
+++ libs/computed-resource/src/lib/computed-resource.ts
@@ -6,6 +6,7 @@ import {
   ResourceStatus,
   Signal,
 } from '@angular/core';
+import { k } from '@angular/core/weak_ref.d-Bp6cSy-X';
 
 const resA: Resource<string | undefined> = resource({
   loader: () => Promise.resolve('A'),
@@ -18,12 +19,19 @@ const map = {
   a: resA,
   b: resB,
 };
+const output: {
+  a: ReturnType<(typeof map)['a']['value']>;
+  b: ReturnType<(typeof map)['b']['value']>;
+} = {
+  a: resA.value(),
+  b: resB.value(),
+};
 
 type ResourceMap = {
   [key: string]: Resource<unknown>;
 };
 
-export function computedResource(resources: ResourceMap) {
+export function computedResourceOld(resources: ResourceMap) {
   type KEY = Exclude<keyof typeof resources, number>;
   type VALS = (typeof resources)[KEY] extends Resource<infer VALS>
     ? VALS
@@ -36,6 +44,7 @@ export function computedResource(resources: ResourceMap) {
     // create a computed value for each of the resources in the map
     for (const [key, resource] of Object.entries(resources)) {
       type VAL = typeof resource extends Resource<infer V> ? V : unknown;
+      // type VAL = ReturnType<typeof resource.value>;
       result[key] = resource.value() as VAL;
     }
 
@@ -51,7 +60,7 @@ export function computedResource(resources: ResourceMap) {
 // { a: string; b: number; } OR
 // { a: string | undefined; b: number | undefined; }
 const check: { a: string | undefined; b: number | undefined } =
-  computedResource(map).value();
+  computedResourceOld(map).value();
 
 console.log(check);
 
@@ -63,6 +72,8 @@ function extractResourceValues<T extends ResourceMap>(
   const values: Partial<{
     [K in keyof T]: T[K] extends Resource<infer V> ? V : never;
   }> = {};
+
+  // type VAL = T[keyof T] extends Resource<infer V> ? V : never };
 
   for (const [key, resource] of Object.entries(resources)) {
     values[key as keyof T] = resource.value() as T[keyof T] extends Resource<
@@ -81,4 +92,67 @@ function extractResourceValues<T extends ResourceMap>(
 }
 const check2 = extractResourceValues(map).value();
 
-console.log(check2);
+type VAL<T> = T[keyof T] extends Resource<infer V> ? V : never;
+
+// const x: Resource<string>;
+
+/*
+{
+  value: () => { [K in keyof T]: T[K] extends Resource<infer V> ? V : never };
+}
+*/
+// function computedResource<T>(resources: {
+type MyResource<T> = Pick<Resource<T>, 'value'>;
+// type MyResource<T> = {
+//   value: () => T;
+// };
+
+function computedResourceX<
+  T extends { [key: string]: Resource<unknown> },
+  O extends { [K in keyof T]: T[K] extends Resource<infer V> ? V : never }
+>(resources: T): MyResource<O> {
+  const value = computed(() => {
+    const result = Object.keys(resources).reduce((acc, key) => {
+      acc[key as keyof O] = resources[key].value() as O[keyof O];
+      return acc;
+    }, {} as Partial<O>);
+
+    return result as O;
+  });
+
+  return {
+    value: value,
+  };
+}
+const xxx = computedResourceX(map);
+xxx.value().a = 'b';
+xxx.value().b = 3;
+
+function computedResource<
+  V,
+  T extends { [K in keyof T]: T[K] extends Resource<infer V> ? V : never }
+>(resources: { [key: string]: Resource<unknown> }): Resource<V> {
+  // }): Resource<{
+  //   [K in keyof T]: T[K] extends Resource<infer V> ? V : never;
+  // }> {
+  const values: Partial<{
+    [K in keyof T]: V;
+  }> = {};
+  for (const [key, resource] of Object.entries(resources)) {
+    values[key as keyof T] = resource.value() as V;
+    // values[key as keyof T] = resource.value() as T[keyof T] extends Resource<
+    //   infer V
+    // >
+    //   ? V
+    //   : never;
+  }
+  return {
+    value: () =>
+      values as {
+        [K in keyof T]: T[K] extends Resource<infer V> ? V : never;
+      },
+  };
+}
+
+const shouldBe = computedResource(map);
+console.log(shouldBe.value()); // should be { a: string | undefined; b: number | undefined; }
